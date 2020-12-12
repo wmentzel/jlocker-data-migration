ebd6af9c3ff642898f342fc0559f2b5e6934df3b
Willi Mentzel <willi.mentzel@gmail.com>
2020-12-12 14:50:49 +0100

    no need for that...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.io.File;
  * This is the main windows of the application. It is displayed right after
  * the login-dialog/create new user dialog.i
  */
-public class Main extends javax.swing.JFrame {
+public class Main {
 
     public static void main(String[] args) {
 
