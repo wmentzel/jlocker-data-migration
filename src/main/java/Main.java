a8d21bfa4368c95869136297b4d03ee68dff35a9
Willi Mentzel <willi.mentzel@gmail.com>
2020-12-13 10:37:55 +0100

    take passwords from input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import com.randomlychosenbytes.jlocker.newformat.NewFormatUtil;
 
+import java.io.Console;
 import java.io.File;
 
 /**
@@ -10,16 +11,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String superUserPassword = "11111111";
-        String limitedUserPassword = "22222222";
+        Console console = System.console();
+
+        String superUserPassword = console.readLine("Super User password: ");
+        String limitedUserPassword = console.readLine("Limited User password: ");
 
         File appDir = Util.getAppDir();
         System.out.println("* program directory is: \"" + appDir.getAbsolutePath() + "\"");
 
-        File oldJLockerDatFile = new File(appDir, "src/test/data/jlocker.dat");
+        File oldJLockerDatFile = new File(appDir, "jlocker.dat");
         OldData oldData = OldFormatUtil.loadData(oldJLockerDatFile, superUserPassword, limitedUserPassword);
 
-        File newJLockerDatFile = new File(appDir, "src/test/data/jlocker.json");
+        File newJLockerDatFile = new File(appDir, "jlocker.json");
 
         NewData newData = Converter.convert(oldData, superUserPassword, limitedUserPassword, oldData.users.get(0).getSecretKeys(superUserPassword).getX());
 
