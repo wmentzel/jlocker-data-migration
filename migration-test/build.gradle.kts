8c5c1a2f6a2aa487c99aaabaff4fab552fc1dc3f
Willi Mentzel <willi.mentzel@gmail.com>
2024-03-16 16:56:00 +0100

    Revert bump and add comment

--- migration-test/build.gradle.kts
+++ migration-test/build.gradle.kts
@@ -19,15 +19,15 @@ dependencies {
     implementation("org.jgrapht:jgrapht-ext:1.0.0") // leave at 1.0.0, breaking changes
     implementation("org.jgrapht:jgrapht-core:1.0.0") // leave at 1.0.0, breaking changes
 
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.google.guava:guava:33.0.0-jre")
-    implementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.1")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
-
+    // Don't bump those! Higher versions will not be compatible with Java 7.
+    implementation("com.google.code.gson:gson:2.3.1")
+    implementation("com.google.guava:guava:23.0")
+    implementation("jakarta.xml.bind:jakarta.xml.bind-api:2.3.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.0")
     testImplementation(kotlin("test"))
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.27.0")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
 }
 
 tasks.withType<Test>().configureEach {
